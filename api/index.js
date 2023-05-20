d515c36e4975a604e0044de8c33c4f1e8e30029d
ayushdhiman-py <ayushdhiman0107@gmail..com>
2023-05-20 13:37:59 +0530

    back to working may work

--- api/index.js
+++ api/index.js
@@ -46,7 +46,8 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/client/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname1, "client", "dist", "index.html"));
+    res.sendFile(path.join(__dirname1, "../client", "dist", "index.html"));
+    console.log(path.join(__dirname1, "../client", "dist", "index.html"))
   });
 } else {
   app.get("/", (req, res) => {
