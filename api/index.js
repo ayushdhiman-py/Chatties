426cc00faf3175cf5f8bff1de24f2e5d75c89273
ayushdhiman-py <ayushdhiman0107@gmail..com>
2023-05-20 13:59:38 +0530

    back to working may work

--- api/index.js
+++ api/index.js
@@ -44,7 +44,7 @@ async function getUserDataFromReq(req) {
 
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/dist")));
+  app.use(express.static(path.join(__dirname1, "../client/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname1, "../client", "dist", "index.html"));
     console.log(path.join(__dirname1, "../client", "dist", "index.html"))
