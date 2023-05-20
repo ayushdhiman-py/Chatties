7656cb65423943379d8db66be9f9bdad3453737d
ayushdhiman-py <ayushdhiman0107@gmail..com>
2023-05-20 13:00:43 +0530

    back to working

--- api/index.js
+++ api/index.js
@@ -44,10 +44,10 @@ async function getUserDataFromReq(req) {
 
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname1, "client", "dist", "index.html"));
-  });
+  // app.use(express.static(path.join(__dirname1, "/client/dist")));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname1, "client", "dist", "index.html"));
+  // });
   app.get("/", (req, res) => {
     res.json("ok tested");
   });
