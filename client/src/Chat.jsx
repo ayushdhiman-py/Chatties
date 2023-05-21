24d966f3398709cedeebf6d90bb924b610b841c4
ayushdhiman-py <ayushdhiman0107@gmail..com>
2023-05-21 13:44:24 +0530

    may work this time ws added changes vallues

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -108,7 +108,7 @@ export default function Chat() {
     axios.get("/people").then((res) => {
       const offlinePeopleArr = res.data
         .filter((p) => p._id !== id)
-        .filter((p) => !Object.keys(onlinePeople).includes(p._id));
+        .filter((p) => !Object.values(onlinePeople).includes(p._id));
       const offlinePeople = {};
       console.log(offlinePeopleArr)
       offlinePeopleArr.forEach((p) => {
