8afd5654b31b5fb11e290d260bb8cc98400adb65
ayushdhiman-py <ayushdhiman0107@gmail..com>
2023-05-21 13:53:31 +0530

    may work this time ws added changes values _id

--- api/index.js
+++ api/index.js
@@ -69,7 +69,7 @@ app.get("/messages/:userId", async (req, res) => {
 
 // here we get all the people
 app.get("/people", async (req, res) => {
-  const users = await UserModel.find({}, { _id: 1, username: 1 });
+  const users = await UserModel.find({}, { '_id': 1, username: 1 });
   res.json(users);
 });
 
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -108,14 +108,12 @@ export default function Chat() {
     axios.get("/people").then((res) => {
       const offlinePeopleArr = res.data
         .filter((p) => p._id !== id)
-        .filter((p) => !Object.values(onlinePeople).includes(p._id));
+        .filter((p) => !Object.keys(onlinePeople).includes(p._id));
       const offlinePeople = {};
-      console.log(offlinePeopleArr)
       offlinePeopleArr.forEach((p) => {
         offlinePeople[p._id] = p;
       });
       setOfflinePeople(offlinePeople);
-      console.log(offlinePeople, "ofline");
     });
   }, [onlinePeople]);
 
